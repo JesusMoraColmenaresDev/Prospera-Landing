4068b7db388db34d9b3339bdda481c5aeb7f2857
Jesus Miguel Mora Colmenares <jesusmiguelmora.dev@gmail.com>
2025-04-22 23:11:50 -0400

    corrected about view design

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,19 @@
+import { Check, User, UserRoundCheck, Users } from 'lucide-react'
 import React from 'react'
 
 export default function AboutUspage() {
   return (
     <div className='flex gap-8 items-center '>
       <div className='flex flex-col gap-2 border-l-2'>
-        <p className='text-[#183657] font-extrabold text-7xl  pl-4 py-4'>Acerca de nosotros:</p>
-        <p className='font-semibold text-3xl ml-4'>Nuestra mision: <span className='text-[#183657] text-3xl font-extrabold'>Tu exito financiero</span></p>
+      <div className='flex items-center pl-2 gap-8'>
+        <p className='text-[#183657] font-extrabold text-7xl  pl-4 py-4'>Acerca de nosotros</p>
+        <UserRoundCheck className='text-[#183657] size-24' strokeWidth={2.1}/>
+      </div>
+
+        
+          <p className='font-semibold text-3xl mt-4 ml-4'>Nuestra mision: <span className='text-[#183657] text-3xl font-extrabold'> Tu exito financiero</span></p>
+
+        
         <p className='text-2xl gap-6 text-[#3A3A3A] opacity-80 ml-4 mt-8'>Nos dedicamos a brindar orientacion financiera experta adaptada a tus necesidades únicas.
           Nuestros profesionales con experiencia están comprometidos a ayudarte a alcanzar un éxito
           financiero duradero mediante estrategias personalizadas y un apoyo constante
